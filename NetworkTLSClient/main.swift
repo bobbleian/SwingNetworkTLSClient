d52126eb7dc2f3bc8efebd8f10d592c12a172ac6
Ian Campbell <bobbleian@gmail.com>
2019-10-18 10:48:41 -0700

    Initial Commit

---
+++ NetworkTLSClient/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NetworkTLSClient
+//
+//  Created by Ian Campbell on 2019-10-18.
+//  Copyright © 2019 Ian Campbell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
